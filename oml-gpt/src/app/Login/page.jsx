1f5acd9a9ea17ef6627fbfdb6b4a10170e3b203f
Warren Pagsuguiron <66290894+warrenwallis@users.noreply.github.com>
2023-11-23 13:14:59 -0800

    Sign In Authentication and Redirect to Home
    Be able to sign in with email and password used to create account.
    Authentication allows user to go to home page.

--- oml-gpt/src/app/Login/page.jsx
+++ oml-gpt/src/app/Login/page.jsx
@@ -1,15 +1,41 @@
 'use client'
-import React, { useState } from "react";
-import Login from "./login"
-import Sidebar from "@/components/sidebar"
+import React, { useState, useEffect } from 'react';
+
+import Login from './login'
+import Sidebar from '@/components/sidebar'
+
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from '@/lib/firebase/firebase'
+
+import { redirect } from 'next/navigation';
+import ButtonWithLink from '@/components/ButtonWithLink';
 
 export default function Page() {
     const [loggedUser, setLoggedUser] = useState(null)
 
+    useEffect(() => {
+        const listen = onAuthStateChanged(auth, (user) => {
+            if (user) {
+                setLoggedUser(user)
+            }
+            else {
+                setLoggedUser(null)
+            }
+        })
+    })
+
     return (
         <main>
-            <Login user={ setLoggedUser } />
-            {/* <Sidebar initialUser={null}/> */}
+            {loggedUser ? (
+                <>
+                    <p>Hello {loggedUser.email}</p>
+                    <ButtonWithLink href='/home'>Go To Home</ButtonWithLink>
+                </>
+            ) : (
+                <Login />
+            )}
+
+            
         </main>
     )
 }
