a4867088d4b29140dfe6be5c180592bd0ff1e890
ArvinDing <dingarvin@gmail.com>
2023-11-23 13:38:57 -0800

    Added persistent messages on the frontend + form

--- oml-gpt/src/app/Home/page.jsx
+++ oml-gpt/src/app/Home/page.jsx
@@ -1,24 +1,52 @@
+"use client";
+
+import React, { useState } from 'react';
+
 const Page = () => {
+  const [messages, setMessages] = useState([]);
+
+  const handleAddMessage = (newMessage) => {
+    setMessages([...messages, newMessage]);
+  };
 
   return (
     <>
       <div>Home</div>
-    
-      <Form />
-      
-
+        <Chat messages={messages} />
+        <Form onAddMessage={handleAddMessage}/>
     </>
     
   )
 }
 
-const Form = () => {
+const Form = ({ onAddMessage }) => {
+  const [inputValue, setInputValue] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddMessage(inputValue);
+    setInputValue('');
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="Type here" className="input w-full max-w-xs" />
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Type here" value={inputValue} className="input w-full max-w-xs"   onChange={(e) => setInputValue(e.target.value)}/>
       <button className="btn">Button</button>
     </form>
   )
 }
+
+
+const Chat = ({ messages }) => {
+  return (
+    <div className="chat">
+      {messages.map((message, index) => (
+        <div key={index} className="message">
+          {message}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Page
