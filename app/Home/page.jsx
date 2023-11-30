a4952ba79f747bef16757b849bef203fb229c58f
Jonah Kim <jonahkim03@gmail.com>
2023-11-30 10:37:56 -0800

    added code from other branches

--- app/Home/page.jsx
+++ app/Home/page.jsx
@@ -1,24 +1,73 @@
+'use client';
+
+import React, { useState } from 'react';
+import './styles.css';
+
 const Page = () => {
+	const [messages, setMessages] = useState([]);
 
-  return (
-    <>
-      <div>Home</div>
-    
-      <Form />
-      
+	const [currentPage, setCurrentPage] = useState('Home'); // Initial page
 
-    </>
-    
-  )
-}
+	const handlePageChange = (page) => {
+		setCurrentPage(page);
+		setMessages([]);
+	};
 
-const Form = () => {
-  return (
-    <form>
-      <input type="text" placeholder="Type here" className="input w-full max-w-xs" />
-      <button className="btn">Button</button>
-    </form>
-  )
-}
+	const handleAddMessage = (newMessage, person) => {
+		setMessages([...messages, { person, content: newMessage }]);
+	};
 
-export default Page
+	return (
+		<div className='page-container'>
+			<div className='sidebar'>
+				<button onClick={() => handlePageChange('Home')}>Home</button>
+				<button onClick={() => handlePageChange('Support')}>Support</button>
+				{/* Add more buttons for other chat pages */}
+			</div>
+			<div className='chat-container'>
+				<div>{currentPage}</div>
+				<Chat messages={messages} />
+				<Form onAddMessage={handleAddMessage} />
+			</div>
+		</div>
+	);
+};
+
+const Form = ({ onAddMessage }) => {
+	const [inputValue, setInputValue] = useState('');
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		onAddMessage(inputValue, 'You');
+		setInputValue('');
+	};
+
+	const handleKeyDown = (e) => {
+		if (e.key === 'Enter') {
+			e.preventDefault();
+			handleSubmit(e);
+		}
+	};
+
+	return (
+		<form onSubmit={handleSubmit} style={{ display: 'flex', alignItems: 'center', paddingBottom: '50px', paddingLeft: '50px' }}>
+			<input type='text' placeholder='Type here' value={inputValue} className='input w-full' onChange={(e) => setInputValue(e.target.value)} />
+			<button className='btn'>Button</button>
+		</form>
+	);
+};
+
+const Chat = ({ messages }) => {
+	return (
+		<div className='chat'>
+			{messages.map((message, index) => (
+				<div key={index} className='message'>
+					<strong>{message.person}: </strong>
+					{message.content}
+				</div>
+			))}
+		</div>
+	);
+};
+
+export default Page;
