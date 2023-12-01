77e88f3ddcb575ace08a30b43d0ba134229dd6d8
Akash Pulinthanathu <akashpulinthanathu@gmail.com>
2023-11-30 20:41:47 -0800

    added ability to dynamically add tabs

--- app/components/NavigationTab.jsx
+++ app/components/NavigationTab.jsx
@@ -1,25 +1,61 @@
+// Import necessary modules
 'use client';
+import { useState } from 'react';
 import signout from '@/services/firebase/signout';
 import Link from 'next/link';
 
-const NavigationTab = () => {
-	return (
-		<div className='flex flex-col fixed top-0 left-0 h-full items-center border-r-2 border-slate-100 justify-between'>
-			<div className=''>
-				<Link href='/home'>
-					<p className='p-4 cursor-pointer hover:bg-sky-50 w-40 text-center'>Home</p>
-				</Link>
-				<Link href='/support'>
-					<p className='p-4 cursor-pointer hover:bg-sky-50 w-40 text-center'>Support</p>
-				</Link>
-			</div>
-			<Link href='/'>
-				<p onClick={signout} className='p-4 cursor-pointer hover:bg-sky-50 w-40 text-center'>
-					Log Out
-				</p>
-			</Link>
-		</div>
-	);
+const NavigationTab = ({ tabs, setTabs, setSelectedTab }) => {
+  // State to manage the input value for the new tab
+  const [newTabName, setNewTabName] = useState('');
+
+  // Function to add a new tab
+  const addNewTab = () => {
+    if (newTabName.trim() !== '') {
+      const newTab = { label: newTabName, link: '/home' };
+      setTabs([...tabs, newTab]);
+      setNewTabName(''); // Clear the input field after adding a new tab
+    }
+  };
+
+  // Function to handle tab click
+  const handleTabClick = (label) => {
+    setSelectedTab(label);
+  };
+
+  return (
+    <div className='flex flex-col fixed top-0 left-0 h-full items-center border-r-2 border-slate-100 justify-between'>
+      <div className=''>
+        {/* Map over the tabs array to render each tab dynamically */}
+        {tabs.map((tab, index) => (
+          <Link key={index} href={tab.link}>
+            <p
+              onClick={() => handleTabClick(tab.label)}
+              className={`p-4 cursor-pointer hover:bg-sky-50 w-40 text-center`}
+            >
+              {tab.label}
+            </p>
+          </Link>
+        ))}
+      </div>
+      <div className='flex flex-col items-center'>
+        <input
+          type='text'
+          placeholder='Enter tab name'
+          value={newTabName}
+          onChange={(e) => setNewTabName(e.target.value)}
+          className='p-2 mb-2'
+        />
+        <button onClick={addNewTab} className='p-4 cursor-pointer hover:bg-sky-50 w-40 text-center'>
+          Add
+        </button>
+      </div>
+      <Link href='/'>
+        <p onClick={signout} className='p-4 cursor-pointer hover:bg-sky-50 w-40 text-center'>
+          Log Out
+        </p>
+      </Link>
+    </div>
+  );
 };
 
 export default NavigationTab;
--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,24 +1,45 @@
 'use client';
 
-import { useState } from 'react';
+// Import necessary modules
+import { useState, useEffect } from 'react';
 import InputForm from '../components/InputForm';
 import ChatLogs from '../components/ChatLogs';
 import { useUserAuthContext } from '../providers/UserProvider';
+import NavigationTab from '@/app/components/NavigationTab';
+
 
 const Page = () => {
-	const { user } = useUserAuthContext();
-	const [messages, setMessages] = useState([]);
+  const { user } = useUserAuthContext();
+  const [messages, setMessages] = useState([]);
 
-	return (
-		<div className='h-screen flex flex-col justify-between p-5'>
-			<div className=''>
-				<p className='text-2xl font-medium mb-5'>OML Copilot x {user?.email}</p>
-				<ChatLogs user={user} messages={messages} />
-			</div>
+  // Define the initial tabs
+  const initialTabs = [
+    { label: 'Home', link: '/home' },
+    { label: 'Support', link: '/home' },
+  ];
 
-			<InputForm user={user} messages={messages} setMessages={setMessages} />
-		</div>
-	);
+  // State to manage the list of tabs
+  const [tabs, setTabs] = useState(initialTabs);
+  const [selectedTab, setSelectedTab] = useState('Home');
+
+  useEffect(() => {
+    // Do something with the selectedTab whenever it changes
+    console.log(`Selected Tab in Page: ${selectedTab}`);
+  }, [selectedTab]);
+
+  return (
+	<>
+	<NavigationTab tabs={tabs} setTabs={setTabs} setSelectedTab={setSelectedTab} />
+    <div className='h-screen flex flex-col justify-between p-5'>
+      <div className=''>
+        <p className='text-2xl font-medium mb-5'>OML Copilot x {user?.email} - {selectedTab}</p>
+        <ChatLogs user={user} messages={messages} />
+      </div>
+
+      <InputForm user={user} messages={messages} setMessages={setMessages} />
+    </div>
+	</>
+  );
 };
 
 export default Page;
